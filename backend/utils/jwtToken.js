8505f6333340e8b6376e51298346e89b34284379
17-vanshi <vanshikamohindra.vani@gmail.com>
2024-05-05 17:29:18 +0530

    cors options fixes

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -11,6 +11,7 @@ const sendToken=(user, statusCode, res)=>{
             Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000
 ),
         // httpOnly:true
+        secure:true
       };
 
       res.status(statusCode).cookie("authToken", authToken, options).json({
